a43164d7c64f3318d81f7a4910ab20b5905b163f
accounta <>
2019-02-02 00:53:04 +0900

    githubにアップするのに、絶対パス指定していた箇所を相対パスに修正。ファイル読み込み失敗処理も追加してみた

--- routes/mapchange.js
+++ routes/mapchange.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
-/* GET home page. */
+/* GET METHOD*/
 router.get('/', function(req, res, next) {
-  res.sendFile('/Volumes/LaCie/GitHub/初めてのNodejs/googleMapTest/public/html/mapchange.html');
+  // 相対パスを、path.resolveで絶対パスに変換
+  fs.readFile(path.resolve(__dirname, '../public/html/mapchange.html'), {encoding: 'utf8'}, (err, data) => {
+    if(err){
+      res.send('htmlファイルが読み込めませんでした:' + err.path);
+    }
+    res.send(data);
+  })
 });
+
+/* POST METHOD*/
 router.post('/',(req, res) => {
-  console.log('mapchangeのpostアクション開始')
+  // console.log('mapchangeのpostアクション開始')
      var map_comf = require('../public/javascripts/map_conf.json');
      var areaId = req.body.areaId;
-     console.log(areaId);
+     // console.log(areaId);
      var resJson = {};
      for(let json of map_comf){
-       console.log(json.key);
+       // console.log(json.key);
        if(json.key == areaId){
          resJson.center_lat = json.center_lat;
          resJson.center_lng = json.center_lng;
